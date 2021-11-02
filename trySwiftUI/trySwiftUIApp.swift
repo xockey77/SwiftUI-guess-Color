ca6e7b4da52f3c428cc70a988c5f63b6bda48777
Andrey Belov <belov_andrey@hotmail.com>
2021-11-02 20:39:12 +0500

    Initial Commit

---
+++ trySwiftUI/trySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trySwiftUIApp.swift
+//  trySwiftUI
+//
+//  Created by username on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct trySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
